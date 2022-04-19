eab2f39ca9dcf3561b99e8dd1931ec9d8d436b59
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-19 21:12:28 +0300

    видалення

---
+++ less6.java
@@ -0,0 +1,12 @@
+
+
+    public class less6 {
+
+        public static void main(String args[]){
+
+            String s = "Я діма , написав книгу\b";
+            System.out.println(s);
+        }
+    }
+
+
