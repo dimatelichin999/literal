ffced41b82859cdba45793301f241251083079b6
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-19 20:41:24 +0300

    розрив строки

--- less2.java
+++ less2.java
@@ -2,6 +2,7 @@ public class less2 {
 
     public static void main(String args[]){
 
-        
+        String s = "Я діма \nз міста Тернопіль";
+        System.out.println(s);
     }
 }
---
+++ less3.java
@@ -0,0 +1,7 @@
+public class less3 {
+
+    public static void main(String args[]){
+
+        
+    }
+}
