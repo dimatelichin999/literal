199d535df6bbbc93bba586290645894eab2c8f50
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-19 20:43:51 +0300

    табуляція

--- less3.java
+++ less3.java
@@ -2,6 +2,10 @@ public class less3 {
 
     public static void main(String args[]){
 
-        
+        String s = "0\t12\t34\t56\t78";
+        String a = "0\t12\t34\t56\t78";
+        String b = "0\t12\t34\t56\t78";
+        String c = "0\t12\t34\t56\t78";
+        System.out.println(s);
     }
 }
